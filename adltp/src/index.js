831da39290ebe2f680eed5aad9b44a6800ac3eeb
David Stiven <Endercon_shipper@hotmail.com>
2020-08-05 13:24:26 -0500

    Initial commit

---
+++ adltp/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  <div>
+    <p text align="center">Integrantes Adltp</p>
+<table border="1" align="center">
+<tr>
+  <td>Nombres:</td>
+  <td>Victor Alejandro</td>
+  <td>David Stiven</td>
+  <td>Dilan Santiago</td>
+  <td>Sebastian</td>
+</tr>
+<tr>
+  <td>Apellidos:</td>
+  <td>Aristizabal Ospina</td>
+  <td>Diaz Duarte</td>
+  <td>Torres Mejia</td>
+  <td>Villada Cubidez</td>
+</tr>
+<tr>
+  <td>Correo:</td>
+  <td>Alejoospi779@gmail.com</td>
+  <td>Endy34sky@gmail.com</td>
+  <td>Dilan4376@gmail.com</td>
+  <td>Sebastianvicu13@gmail.com</td>
+</tr>
+<tr>
+  <td>Telefono:</td>
+  <td>3005233439</td>
+  <td>3196253526</td>
+  <td>3022788952</td>
+  <td>3053543700</td>
+</tr>
+</table>
+  </div>,
+
+   document.getElementById('root')
+);
+
