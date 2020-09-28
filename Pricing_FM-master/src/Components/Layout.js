5a78a24f3fecf9ce1804b9b0e6e7928e9cc1bf8d
David Stiven <Endercon_shipper@hotmail.com>
2020-09-27 19:26:08 -0500

    Initial Commit 2

---
+++ Pricing_FM-master/src/Components/Layout.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+function Layout(props) {
+    return(
+        <>
+        <Header 
+              companyName="ADLTP"
+              navBar={[
+                {
+                  name: "Facebook",
+                  url: "http://facebook.com",
+                },
+                {
+                  name: "Twitter",
+                  url: "http://twitter.com",
+                },
+                {
+                  name: "Instagram",
+                  url: "https://www.instagram.com/elcossioooo/?hl=es-la",
+                },
+                {
+                  name: "Youtube",
+                  url: "https://www.youtube.com/channel/UCugoifEZzVuxq7sKgMvyaNQ",
+                },
+              ]}
+              />
+              {props.children}
+        <Footer />
+        </>
+    )
+}
+
+export default Layout
